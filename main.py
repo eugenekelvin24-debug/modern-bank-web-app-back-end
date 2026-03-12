21a51ff7c406f9f7f3405fb470c3ec6de6bd5fda
Kelvin <eugenekelvin24@gmail.com>
2026-03-12 22:06:42 +0100

    TrustBank backend

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+accounts = [
+    {
+      "id": "acc_1",
+      "label": "Savings Account",
+      "type": "Savings",
+      "balance": 24500.00,
+      "trend": "+2.5%",
+      'currency': "$",
+      'accountNumber': "**** 8829",
+      'color': "bg-indigo-600",
+    },
+    {
+      "id": "acc_2",
+      "label": "Checking Account",
+      "type": "Checking",
+      "balance": 1240.50,
+      "trend": "-1.2%",
+      'currency': "$",
+      'accountNumber': "**** 1102",
+    },
+    {
+      "id": "acc_3",
+      "label": "Investment Portfolio",
+      "type": "Investment",
+      "balance": 8900.00,
+      "trend": "+5.8%",
+      'currency': "$",
+      'accountNumber': "**** 4432",
+      'color': "bg-amber-600",
+    }
+  ]
+
+@app.get("/")
+def home():
+    return{"message": "Trustbank API is running"}
+
+@app.get("/accounts")
+def get_accounts():
+    return accounts
